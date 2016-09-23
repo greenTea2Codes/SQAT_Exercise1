6a1134be75b6434a57646719fa7ea845e0ecf94f
somename <someemail>
2016-09-23 15:01:32 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -1,15 +1,28 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestBowling {
 
+	Frame test;
+	int firstThrow = 0;
+	int secondThrow = 0;
+	@Before
+	public void setUp(){
+		test = new Frame(firstThrow, secondThrow);
+	}
 	@Test
 	public void testFrame_score() {
-		int firstThrow = 4;
-		int secondThrow = 5;
-		Frame test = new Frame( firstThrow, secondThrow );
+		//int firstThrow = 4;
+		//int secondThrow = 5;
+		//Frame test = new Frame( firstThrow, secondThrow );
+		
 		assertEquals(9, test.score());
+	}
+	@Test
+	public void testFrame_isStrike(){
+		
 	}
 
 }
