d77bfd8824ab904fa4a3c461cfff1ba9b67c3830
dachen <dachen@STUPID-PC712.stnet.local>
2016-09-23 16:40:49 +0300

    Exercise Finished

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//Finish time:
-//ID:
+//Finish time:16:37
+//ID:142
 
 public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
